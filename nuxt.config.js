aaf1464741aa9684ece30ba79171e01e1b062547
Sho Yamane <sns.yamane@gmail.com>
2018-07-23 06:43:50 +0900

    20180722

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,17 +40,34 @@ module.exports = {
   plugins: ['~plugins/element-ui', '~/plugins/mixin'],
   css: ['element-ui/lib/theme-chalk/index.css'],
   generate: {
+    interval: 500,
     routes: () => {
-      console.log('tets')
-      return axios
-        .get(
-          `https://sho-yamane.mixh.jp/sample/index.php/wp-json/wp/v2/posts?per_page=99`
-        )
-        .then(res => {
-          return res.data.map(post => {
-            return '/post/' + post.id
+      const endpoint =
+        'https://sho-yamane.mixh.jp/sample/index.php/wp-json/wp/v2'
+
+      return Promise.all([
+        axios.get(`${endpoint}/post_all_id`),
+        axios.get(`${endpoint}/page_all_slug`)
+      ]).then(data => {
+        const posts = data[0]
+        const pages = data[1]
+        return posts.data
+          .map(post => {
+            return {
+              route: '/post/' + post.id,
+              payload: post
+            }
           })
-        })
+          .concat(
+            pages.data.map(page => {
+              return {
+                route: page.slug,
+                payload: page
+              }
+            })
+          )
+          .concat(['/'])
+      })
     }
   }
 }
